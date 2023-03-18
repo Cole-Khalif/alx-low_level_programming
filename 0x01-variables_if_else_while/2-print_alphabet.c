68ee17e50956ec9aaa72ee16826dd9f13a37e84f
Cole-Khalif <echukucole@gmail.com>
2023-03-18 12:37:18 -0700

    computed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -main function
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	printf("\n");
+	return (0);
+}
